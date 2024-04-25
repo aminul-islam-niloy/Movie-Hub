c1bd1e2483c9e163653fe6649c2acf51a1d56023
aminul-islam-niloy <niloyftaminul@gmail.com>
2024-04-25 15:35:10 +0600

    Filtering is implemented

--- MoviesFair/Areas/Customer/Controllers/HomeController.cs
+++ MoviesFair/Areas/Customer/Controllers/HomeController.cs
@@ -19,25 +19,42 @@ namespace MoviesFair.Areas.Customer.Controllers
 
         public IActionResult Index()
         {
+            var moviesQuery = _context.Movies.Include(c => c.Genre).Include(c => c.Category).AsQueryable();
 
-            ViewData["GenreTypeSearchId"] = new SelectList(_context.Movies.ToList(), "Id", "Genre");
+            var movies = moviesQuery.ToList();
 
-            return View(_context.Movies.Include(c => c.Genre).Include(c => c.Category).ToList());
+            // Pass genre list to the view
+            ViewData["GenreTypeSearchId"] = new SelectList(_context.Genres.ToList(), "Id", "GenreName");
+
+            return View(movies);
         }
 
 
 
         [HttpPost]
-        public IActionResult Index(string searchString)
+        public IActionResult Index(string searchString, int genreId = 0)
         {
-            var movies = _context.Movies.Include(c => c.Genre).Include(c => c.Category).ToList();
+            var moviesQuery = _context.Movies.Include(c => c.Genre).Include(c => c.Category).AsQueryable();
 
+            // Filter by genre if selected
+            if (genreId != 0)
+            {
+                moviesQuery = moviesQuery.Where(m => m.GenreId == genreId);
+            }
+
+            // Filter by search string
             if (!string.IsNullOrEmpty(searchString))
             {
-                // Filter products based on search string
-                movies = movies.Where(p => p.Name.Contains(searchString)).ToList();
+                moviesQuery = moviesQuery.Where(m => m.Name.Contains(searchString));
             }
+
+            var movies = moviesQuery.ToList();
+
+            // Pass genre list to the view
+            ViewData["GenreTypeSearchId"] = new SelectList(_context.Genres.ToList(), "Id", "GenreName");
+
             return View(movies);
         }
+
     }
-    }
+}
