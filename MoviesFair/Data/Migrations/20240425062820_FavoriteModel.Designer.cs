eeeff8d10b76dcb169e5998fec101f6e827bcf05
aminul-islam-niloy <niloyftaminul@gmail.com>
2024-04-25 12:31:23 +0600

    Fovorite list add

--- MoviesFair/Data/Migrations/20240425062820_FavoriteModel.Designer.cs
+++ MoviesFair/Data/Migrations/20240425062820_FavoriteModel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MoviesFair.Data;
 
@@ -11,9 +12,10 @@ using MoviesFair.Data;
 namespace MoviesFair.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240425062820_FavoriteModel")]
+    partial class FavoriteModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MoviesFair/Models/Favorite.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MoviesFair.Models
+{
+    public class Favorite
+    {
+        public int Id { get; set; }
+
+        // Foreign key for the Movie
+        [Required]
+        public int MovieId { get; set; }
+
+        // Navigation property to the Movie
+        [ForeignKey("MovieId")]
+        public Movie Movie { get; set; }
+
+        // Foreign key for the ApplicationUser (assuming you are using ASP.NET Core Identity)
+        [Required]
+        public string UserId { get; set; }
+
+        // Navigation property to the ApplicationUser
+        //[ForeignKey("UserId")]
+        //public ApplicationUser ApplicationUser { get; set; }
+    }
+}
