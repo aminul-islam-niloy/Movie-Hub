cbd2f00dd9687c51e5a02d74912c307f1bf42fc1
aminul-islam-niloy <niloyftaminul@gmail.com>
2024-09-10 00:16:26 +0600

    Unit test is done

--- TestProject.text/UnitTest1.cs
+++ TestProject.text/UnitTest1.cs
@@ -1,11 +1,32 @@
+
+using Microsoft.AspNetCore.Mvc.Testing;
+
 namespace TestProject.text
 {
-    public class UnitTest1
+    public class UnitTest1: IClassFixture<WebApplicationFactory<Program>>
     {
-        [Fact]
-        public void Test1()
-        {
+        private readonly WebApplicationFactory<Program> _applicationFactory;
 
+        public UnitTest1()
+        {
+            var factory= new WebApplicationFactory<Program>();
+            _applicationFactory=factory;    
+        }
+
+        [Fact]
+        public async void HomeControllerTest()
+        {
+            //areange
+            var client = _applicationFactory.CreateClient();
+
+            //act
+
+            var response = await client.GetAsync("/Customer/Home/Index");
+            int code= (int)response.StatusCode;
+
+            //asert
+
+            Assert.Equal(200, code);
         }
     }
 }
